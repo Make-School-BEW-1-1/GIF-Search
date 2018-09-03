80fa7b43c4e648aaec1658778e6085ab1b4db4da
Severiano Badajoz <sevebadajoz@gmail.com>
2018-09-03 13:15:12 -0700

    Successfully use wrapper

--- app.js
+++ app.js
@@ -4,44 +4,27 @@ var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
 var http = require('http');
+const giphy = require('giphy-api')();
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 app.use(express.static('public'));
 
+
 app.get('/', function (req, res) {
-  console.log(req.query.term)
-  var queryString = req.query.term
-
-  var term = encodeURIComponent(queryString)
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
-
-  http.get(url, function(response) {
-    response.setEncoding('utf8')
-
-    var body = '';
-
-    response.on('data', function (d) {
-      body += d
-    })
-
-    response.on('end', function () {
-      var parsed = JSON.parse(body)
-      res.render('home', {gifs: parsed.data})
-    })
-  })
+  if(req.query.term) {
+      giphy.search(req.query.term, function (err, response) {
+          console.log(`Searching for: ${req.query.term}`)
+          res.render('home', { gifs: response.data })})
+    }
+    else {
+      giphy.trending(function (err, response) {
+          console.log('No keyword, loading trending')
+          res.render('home', { gifs: response.data })})
+      }
 })
 
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-  res.render('hello-gif', {gifUrl: gifUrl})
-});
-
-app.get('/greetings/:name', function (req, res) {
-  var name = req.params.name
-  res.render('greetings', {name: name})
-});
 
 app.listen(3000, function () {
   console.log('Gif Search listening on port localhost:3000!')
